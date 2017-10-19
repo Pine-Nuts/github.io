53111ec4cb25648369517d65cd3af69e59b9665f
csr <1169508335@qq.com>
2017-10-19 21:09:28 +0800

    初始化服务器框架

---
+++ minibar-server/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.Primse = global.Promise;
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/mini-bar',{
+	useMongoClient: true
+},err=>{
+	if(err){
+		console.log(err);
+		console.log('数据库链接失败...')
+	}
+	else{
+		console.log('数据库链接成功')
+	}
+});
+module.exports = {
+	mongoose: mongoose,
+	Schema: Schema
+}
